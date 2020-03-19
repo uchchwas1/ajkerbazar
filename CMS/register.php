66e0259b02b232e782b91943476122b5e94c8d00
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2020-03-20 02:12:06 +0600

    Added files have to customize

---
+++ CMS/register.php
@@ -0,0 +1,33 @@
+<div id="main">
+
+						<!-- Post -->
+							<article class="post">
+								<header>
+									
+								</header>
+								<div id="create_form">
+									<form action="register.php" method="POST">
+
+									<label for="">Food category</label>
+									<input type="text" name="r_name" id="title" />
+
+									
+
+									<label for="">item name</label>
+									<input type="text" name="items" ></textarea>
+
+									<label for="">price</label>
+									<input type="text" name="price" id="title" />
+									
+									<label for="">code</label>
+									<input type="text" name="code" id="title" />
+
+									<input type="hidden" name="publishing" />
+
+									<input type="submit" name="submit" id="submit" value="Publish" />
+									</form>
+								</div>
+								
+							</article>
+
+					</div>
